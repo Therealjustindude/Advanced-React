4bdb5dabfa2bcd26b9b892a3d480da242f84d97e
therealjustindude <therealjustindude@gmail.com>
2021-07-30 14:42:13 -0500

    finished module 1-5

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import 'dotenv/config';
+import { insertSeedData } from './seed-data';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO add in intial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('Connected to database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // show UI for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session)
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
